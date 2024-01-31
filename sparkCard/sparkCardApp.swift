fd89ffb42517a4ebceb579900b4d4225e2526758
aleksandr semenov <aleksandr.semenovsky@gmail.com>
2024-01-31 11:06:14 +0800

    Initial Commit

---
+++ sparkCard/sparkCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkCardApp.swift
+//  sparkCard
+//
+//  Created by Александр Семенов on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct sparkCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
